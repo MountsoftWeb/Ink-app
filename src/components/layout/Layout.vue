0f1c67a9922a462ac0695f04c9c460be06b55e0f
Carlos <458074992@qq.com>
2019-03-31 19:58:32 +0800

    整体布局

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <a-header></a-header>
+            <router-view/>
+
+        <c-footer></c-footer>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+
+</style>
